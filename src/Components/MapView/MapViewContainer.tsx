feb92ede4ae67ad568988ee0835098bf8b70292f
wooknick <wooknick@naver.com>
2021-07-14 02:24:24 +0900

    update MapView

--- src/Components/MapView/MapViewContainer.tsx
+++ src/Components/MapView/MapViewContainer.tsx
@@ -1,6 +1,8 @@
+import "firebase/firestore";
+import * as geofire from "geofire-common";
 import { GoogleAPI } from "google-maps-react";
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import useFirebase from "../../Hooks/useFirebase";
 import MapViewPresenter from "./MapViewPresenter";
 
 interface Props {
@@ -10,16 +12,88 @@ interface Props {
 const MapViewContainer: React.FunctionComponent<Props> = ({
   google,
 }: Props) => {
-  const center = {
-    lat: 37.5,
-    lng: 126.9,
-  };
-  const zoom = 13;
-  useEffect(() => {
-    console.log(google);
-  });
+  const [centerLat, setCenterLat] = useState<number>(37.579863556926874);
+  const [centerLng, setCenterLng] = useState<number>(126.97700881185297);
+  const [radiusInM, setRadiusInM] = useState<number>(8 * 1000);
+  const [zoom, setZoom] = useState<number>(15);
+  const [data, setData] = useState<Array<Heritage>>([]);
 
-  return <MapViewPresenter google={google} />;
+  useEffect(() => {
+    getGeoPosition();
+    // getData();
+  }, []);
+
+  function getGeoPosition() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+      setCenterLat(position.coords.latitude);
+      setCenterLng(position.coords.longitude);
+    });
+  }
+  async function getData() {
+    const firebase = useFirebase();
+    const db = firebase.firestore();
+    // Get All Documents
+    // const snapshot = await db.collection("heritage").get();
+    // snapshot.forEach((doc) => {
+    //   console.log(doc.id, "=>", doc.data());
+    // });
+    const bounds = geofire.geohashQueryBounds(
+      [centerLat, centerLng],
+      radiusInM
+    );
+    const promises = [];
+    for (const b of bounds) {
+      const q = db
+        .collection("heritage")
+        .orderBy("geohash")
+        .startAt(b[0])
+        .endAt(b[1]);
+
+      promises.push(q.get());
+    }
+
+    Promise.all(promises)
+      .then((snapshots) => {
+        const matchingDocs = [];
+        for (const snap of snapshots) {
+          for (const doc of snap.docs) {
+            const location = doc.get("location");
+            const lat = location._lat;
+            const lng = location._long;
+            const distanceInKm = geofire.distanceBetween(
+              [lat, lng],
+              [centerLat, centerLng]
+            );
+            const distanceInM = distanceInKm * 1000;
+            if (distanceInM <= radiusInM) {
+              matchingDocs.push(doc);
+            }
+          }
+        }
+        return matchingDocs;
+      })
+      .then((matchingDocs) => {
+        const newData: Array<Heritage | any> = [];
+        matchingDocs.forEach((doc) => {
+          // console.log(doc);
+          newData.push(doc.data());
+          // console.log(doc.id, "=>", doc.data());
+        });
+        setData(newData);
+      });
+  }
+
+  return (
+    <MapViewPresenter
+      google={google}
+      data={data}
+      centerLat={centerLat}
+      centerLng={centerLng}
+      zoom={zoom}
+    />
+  );
 };
 
 export default MapViewContainer;
--- src/Components/MapView/MapViewPresenter.tsx
+++ src/Components/MapView/MapViewPresenter.tsx
@@ -1,5 +1,12 @@
-import { GoogleAPI, Map } from "google-maps-react";
+import {
+    Circle,
+    GoogleAPI,
+    InfoWindow,
+    Map,
+    Marker
+    } from "google-maps-react";
 import React from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -10,19 +17,43 @@ const Wrapper = styled.div`
 
 interface Props {
   google: GoogleAPI;
+  data: Array<Heritage>;
+  centerLat: number;
+  centerLng: number;
+  zoom: number;
 }
 
 const MapViewPresenter: React.FunctionComponent<Props> = ({
   google,
+  data,
+  centerLat,
+  centerLng,
+  zoom,
 }: Props) => {
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
   return (
     <Wrapper>
       <Map
         google={google}
-        center={{ lat: 37.5, lng: 126.9 }}
-        // // containerStyle={{ position: "relative", height: "100vh" }}
-        // style={{ position: "relative", height: "100%" }}
-      />
+        center={{ lat: centerLat, lng: centerLng }}
+        zoom={zoom}
+      >
+        <Marker
+          position={{ lat: centerLat, lng: centerLng }}
+          label={"me"}
+          visible={true}
+          icon={"/Images/Me.png"}
+        />
+
+        <Marker
+          position={{ lat: centerLat + 0.006, lng: centerLng + 0.005 }}
+          label={"<div>heysdfsdfsd</div>"}
+          visible={true}
+          icon={{ url: "/Images/Heritage.png" }}
+        />
+      </Map>
     </Wrapper>
   );
 };
