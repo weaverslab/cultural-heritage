48d472b7631330a696365625363438e252bf3d57
wooknick <wooknick@naver.com>
2021-07-15 05:47:13 +0900

    update Loader

--- src/Components/Loader/Loader.tsx
+++ src/Components/Loader/Loader.tsx
@@ -1,18 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+interface StyledProps {
+  width: string;
+  height: string;
+}
+
+const Wrapper = styled.div<StyledProps>`
   position: absolute;
-  width: 100%;
-  height: 100%;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
   background-color: #e6e690;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const Loader: React.FunctionComponent = () => {
-  return <Wrapper>준비중입니다.</Wrapper>;
+interface Props {
+  width?: string;
+  height?: string;
+}
+
+const Loader: React.FunctionComponent<Props> = ({
+  width = "100%",
+  height = "100%",
+}: Props) => {
+  return (
+    <Wrapper width={width} height={height}>
+      준비중입니다.
+    </Wrapper>
+  );
 };
 
 export default Loader;
