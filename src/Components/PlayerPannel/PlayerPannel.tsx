cdee0c35c142de5950400debf4931ac3188fe596
wooknick <wooknick@naver.com>
2021-07-17 04:14:11 +0900

    update overflow layout

--- src/Components/PlayerPannel/PlayerPannel.tsx
+++ src/Components/PlayerPannel/PlayerPannel.tsx
@@ -10,7 +10,13 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   align-items: center;
-  padding: 24px 16px;
+  padding: 16px 16px;
+  overflow: hidden;
+`;
+
+const OverflowWrapper = styled.div`
+  width: 100%;
+  height: 100%;
   overflow: scroll;
 `;
 
@@ -38,22 +44,24 @@ const PlayerPannel: React.FunctionComponent<Props> = ({
 }: Props) => {
   return (
     <Wrapper>
-      {guideData.map((guide: Guide) => {
-        return (
-          <Card
-            key={guide.id}
-            selected={selctedGuide?.id === guide.id}
-            onClick={() => {
-              setSelectedGuide(guide);
-            }}
-          >
-            {guide.title}
-          </Card>
-        );
-      })}
-      <Link to={{ pathname: "/" }}>
-        <Card>뒤로가기</Card>
-      </Link>
+      <OverflowWrapper>
+        {guideData.map((guide: Guide) => {
+          return (
+            <Card
+              key={guide.id}
+              selected={selctedGuide?.id === guide.id}
+              onClick={() => {
+                setSelectedGuide(guide);
+              }}
+            >
+              {guide.title}
+            </Card>
+          );
+        })}
+        <Link to={{ pathname: "/" }}>
+          <Card>뒤로가기</Card>
+        </Link>
+      </OverflowWrapper>
     </Wrapper>
   );
 };
