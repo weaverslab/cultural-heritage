ea24092847eadc27dca917a4609d7678002773ad
wooknick <wooknick@naver.com>
2021-07-24 04:35:15 +0900

    update Loader

--- src/Components/Loader/Loader.tsx
+++ src/Components/Loader/Loader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Hr from "../Hr";
 
 interface StyledProps {
   width: string;
@@ -10,24 +11,45 @@ const Wrapper = styled.div<StyledProps>`
   position: absolute;
   width: ${(props) => props.width};
   height: ${(props) => props.height};
-  background-color: #e6e690;
+  background-color: white;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
+const Pattern = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url("/images/patterns.png");
+  background-repeat: repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  ${(props) => props.theme.fonts.bigTitle};
+`;
+
+const Message = styled.div`
+  margin-top: 1rem;
+`;
+
 interface Props {
   width?: string;
   height?: string;
+  message?: string;
 }
 
 const Loader: React.FunctionComponent<Props> = ({
   width = "100%",
   height = "100%",
+  message = "로딩중입니다",
 }: Props) => {
   return (
     <Wrapper width={width} height={height}>
-      준비중입니다.
+      <Pattern>
+        <Hr width={"100px"} />
+        <Message>{message}</Message>
+      </Pattern>
     </Wrapper>
   );
 };
